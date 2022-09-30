52395c88a444868e8fc4ccd74fe1654a18c4a676
shade2903 <nighshade2903@gmail.com>
2022-09-30 13:44:07 +0300

    Initial commit

---
+++ src/main/java/com/haiduk/SpringSecurity/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.haiduk.SpringSecurity.config;
+
+import com.haiduk.SpringSecurity.security.AuthProviderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ private final AuthProviderImpl authProvider;
+
+ @Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(authProvider);
+    }
+
+}
---
+++ src/main/java/com/haiduk/SpringSecurity/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package com.haiduk.SpringSecurity.repositories;
+
+import com.haiduk.SpringSecurity.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
